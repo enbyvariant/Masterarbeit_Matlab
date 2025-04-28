2c8479fd84c6ad7df578b0b9d4f71d02fdcd925b
Lara Icking <lar.icking@srabmit.sra.uni-hannover.de>
2025-04-28 15:51:04 +0200

    replacing switch cases with zero rows for missing boundaries

--- Interior_Points_Init.m
+++ Interior_Points_Init.m
@@ -1,4 +1,4 @@
-function [x, sl, su, y, wl, wu, bound] = Interior_Points_Init(H, A, b, c, xl, xu, x, y)
+function [x, sl, su, y, wl, wu, bound_l, bound_u] = Interior_Points_Init(H, A, b, c, xl, xu, x, y)
 % Calculate initial iterate of Interior Point QP Method 
 
     n = size(A, 2);
@@ -30,7 +30,7 @@ function [x, sl, su, y, wl, wu, bound] = Interior_Points_Init(H, A, b, c, xl, xu
     Su1 = diag(ones(n,1)./su);
     Wu = diag(wu);
 
-    % calculate affine steps according to which bound of x exists
+    % calculate affine step according to which bound of x exists
 
     mat = [H zeros(n,2*n) A' bound_l*eye(n) -bound_u*eye(n);
         zeros(n) bound_l*Sl1*Wl zeros(n,n+m) -bound_l*eye(n) zeros(n);
@@ -39,59 +39,20 @@ function [x, sl, su, y, wl, wu, bound] = Interior_Points_Init(H, A, b, c, xl, xu
         bound_l*eye(n) -bound_l*eye(n) zeros(m+3*n);
         -bound_u*eye(n) zeros(n) -bound_u*eye(n) zeros(m+2*n)];
     omega = [H*x + c - A'*y - wl + wu; Wl*e; Wu*e; A*x - b; bound_l*(x -xl -sl); bound_u*(-x +xu -su)];
-    switch bound
-        case "lu"
-            mat = [H zeros(n,2*n) A' eye(n) -eye(n);
-                zeros(n) Sl1*Wl zeros(n,n+m) -eye(n) zeros(n); 
-                zeros(n,2*n) Su1*Wu zeros(n, m+n) -eye(n); 
-                A zeros(m,m+4*n);
-                eye(n) -eye(n) zeros(m+3*n); 
-                -eye(n) zeros(n) -eye(n) zeros(m+2*n)];
-            omega = [H*x + c - A'*y -wl + wu; Wl*e; Wu*e; A*x - b; (x -xl -sl); (-x +xu -su)];
-            sol = linsolve(mat, -omega);
-            del_x = sol(1:n);
-            del_sl = sol(n+1:2*n);
-            del_su = sol(2*n+1:3*n);
-            del_wl = sol(3*n+m+1:4*n+m);
-            del_wu = sol(4*n+m+1:5*n+m);
-
-            sl = max(abs(sl + del_sl),e);
-            su = max(abs(su + del_su),e);
-            wl = max(abs(wl + del_wl),e);
-            wu = max(abs(wu + del_wu),e);
-        case "l"
-            mat = [H zeros(n,n) A' eye(n);
-                zeros(n) Sl1*Wl zeros(n,m) -eye(n); 
-                A zeros(m,m+2*n);
-                eye(n) -eye(n) zeros(m+n)];
-            omega = [H*x + c - A'*y -wl; Wl*e; A*x -b; x-xl-sl];
-            sol = linsolve(mat, -omega);
-            del_x = sol(1:n);
-            del_sl = sol(n+1:2*n);
-            del_wl = sol(2*n+m+1:3*n+m);
-
-            sl = max(abs(sl + del_sl),e);
-            wl = max(abs(wl + del_wl),e);
-        case "u"
-            mat = [H zeros(n,n) A' -eye(n);
-                zeros(n) Su1*Wu zeros(n,m) -eye(n); 
-                A zeros(m,m+2*n);
-                -eye(n) eye(n) zeros(m+n)];
-            omega = [H*x + c - A'*y -wu; Wu*e; A*x -b; -x+xu-su];
-            sol = linsolve(mat, -omega);
-            del_x = sol(1:n);
-            del_su = sol(n+1:2*n);
-            del_wu = sol(2*n+m+1:3*n+m);
-
-            su = max(abs(su + del_su),e);
-            wu = max(abs(wu + del_wu),e);
-        otherwise
-            mat = [H A'; A zeros(m)];
-            omega = [H*x + c - A'*y; A*x - b];
-            sol = linsolve(mat, -omega);
-            del_x = sol(1:n);
-    end
+    sol = omega\mat;
+    
+    del_x = sol(1:n);
+    del_sl = sol(n+1:2*n);
+    del_su = sol(2*n+1:3*n);
+    del_wl = sol(3*n+m+1:4*n+m);
+    del_wu = sol(4*n+m+1:5*n+m);
+    
     % ensure positivity of x, s and w (interiority)
-    x = max(abs(x + del_x),e);
+    sl = max(abs(sl + del_sl),e);
+    su = max(abs(su + del_su),e);
 
+    wl = max(abs(wl + del_wl),e);
+    wu = max(abs(wu + del_wu),e);
+
+    x = max(abs(x + del_x),e);
 end
