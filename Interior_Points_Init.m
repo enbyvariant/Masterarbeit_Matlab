406f36b57727d7beee0938ab39496ca8024b2754
Loki <lar.icking@srabmit.sra.uni-hannover.de>
2025-08-21 14:50:31 +0200

    using structs in QP method

--- Interior_Points_Init.m
+++ Interior_Points_Init.m
@@ -1,142 +1,109 @@
-function [x, sl, su, tl, tu, y, wl, wu, zl, zu, bound_xl,bound_xu, bound_cl, bound_cu] = Interior_Points_Init(H, A, b, c, xl, xu, cl, cu, C)
+function [it] = Interior_Points_Init(nlp,dim)
 % Calculate initial iterate of Interior Point QP Method 
 
-    n = size(A, 2);
-    m = size(A, 1);
-    r = size(C,1);
+    n = dim.n;
+    m = dim.m;
+    r = dim.r;
     en = ones(n,1);
+    er = ones(r,1);
 
-    wl = ones(n,1);
-    wu = ones(n,1);
+    it.wl = ones(n,1);
+    it.wu = ones(n,1);
 
-    zl = ones(r,1);
-    zu = ones(r,1);
+    it.zl = ones(r,1);
+    it.zu = ones(r,1);
 
-    x = rand(n,1);
-    y = rand(m,1);
+    it.x = rand(n,1);
+    it.y = rand(m,1);
 
     % cancel lines where x-component has no upper or no lower bound
     index_l = ones(1,n);
     index_u = ones(1,n);
     for i = 1:n
-        if xl(i) < -10^3
-            xl(i) = 0;
-            wl(i) = 0; 
+        if nlp.xl(i) < -10^3
+            nlp.xl(i) = 0;
+            it.wl(i) = 0; 
             index_l(i) = 0;
         end
-        if xu(i) > 10^3
-            xu(i) = 0;
-            wu(i) = 0;
+        if nlp.xu(i) > 10^3
+            nlp.xu(i) = 0;
+            it.wu(i) = 0;
             index_u(i) = 0;
         end
     end
     index_l = find(index_l);
     index_u = find(index_u);
-    bound_xl = sparse(index_l, index_l, ones(size(index_l)), n, n);
-    bound_xu = sparse(index_u, index_u, ones(size(index_u)), n, n);
+    it.bound_xl = sparse(index_l, index_l, ones(size(index_l)), n, n);
+    it.bound_xu = sparse(index_u, index_u, ones(size(index_u)), n, n);
 
-    sl = bound_xl*ones(n,1);
-    su = bound_xu*ones(n,1);
+    it.sl = it.bound_xl*ones(n,1);
+    it.su = it.bound_xu*ones(n,1);
+
+    %prepare equation system
     Sl1 = zeros(n);
     Su1 = zeros(n);
     for i = 1:n
-        if sl(i) ~= 0
-            Sl1(i,i) = 1/sl(i);
+        if it.sl(i) ~= 0
+            Sl1(i,i) = 1/it.sl(i);
         end
-        if su(i) ~= 0
-            Su1(i,i) = 1/su(i);
+        if it.su(i) ~= 0
+            Su1(i,i) = 1/it.su(i);
         end
     end
-    Wl = diag(wl);
-    Wu = diag(wu);
+    Wl = diag(it.wl);
+    Wu = diag(it.wu);
 
-    % % Differentiate between the case where inequality constraints exist
-    % % or not
-     if r == 0
-        bound_cl = 0;
-        bound_cu = 0;
-        tl = [];
-        tu = [];
-    
-        % helping variables
-        betal = bound_xl*(x-xl-sl);
-        betau = bound_xu*(-x+xu-su);
-        Hphi = H + Sl1*Wl + Su1*Wu;
-
-        % % calculate the affine step with given iterate
-        mat = [Hphi A'; A zeros(m)];
-        omega_1 = -H*x - c + A'*y - bound_xl*Sl1*Wl*betal + bound_xu*Su1*Wu*betau;
-        omega_2 = -(A*x -b);
-        omega = [omega_1; omega_2];
-        sol = mat\omega;
-        del_x = sol(1:n);
-        %del_y = sol(n+1:n+m);
-
-        % calculate all variables of expanded system
-        del_sl = bound_xl*(del_x + betal);
-        del_su = bound_xu*(-del_x + betau);
-        del_wl = bound_xl*(-wl - bound_xl*Sl1*Wl*del_sl);
-        del_wu = bound_xu*(-wu - bound_xu*Su1*Wu*del_su);
-
-        % ensure positivity s and w of the starting iterate
-        sl = bound_xl*max(abs(sl + del_sl),en);
-        su = bound_xu*max(abs(su + del_su),en);
-
-        wl = bound_xl*max(abs(wl + del_wl),en);
-        wu = bound_xu*max(abs(wu + del_wu),en);
-
-    else
         index_cl = ones(1,r);
         index_cu = ones(1,r);
         for i = 1:r
-            if cl(i) < -10^3
-                cl(i) = 0;
-                zl(i) = 0;
+            if nlp.cl(i) < -10^3
+                nlp.cl(i) = 0;
+                it.zl(i) = 0;
                 index_cl(i) = 0;
 
             end
-            if cu(i) > 10^3
-                cu(i) = 0;
-                zu(i) = 0;
+            if nlp.cu(i) > 10^3
+                nlp.cu(i) = 0;
+                it.zu(i) = 0;
                 index_cu(i) = 0;
             end
         end
-        Zl = diag(zl);
-        Zu = diag(zu);
+        Zl = diag(it.zl);
+        Zu = diag(it.zu);
         index_cl = find(index_cl);
         index_cu = find(index_cu);
-        bound_cl = sparse(index_cl, index_cl, ones(size(index_cl)), r, r);
-        bound_cu = sparse(index_cu, index_cu, ones(size(index_cu)), r, r);
+        it.bound_cl = sparse(index_cl, index_cl, ones(size(index_cl)), r, r);
+        it.bound_cu = sparse(index_cu, index_cu, ones(size(index_cu)), r, r);
 
-        tl = bound_cl*ones(r,1);
-        tu = bound_cu*ones(r,1);
+        it.tl = it.bound_cl*ones(r,1);
+        it.tu = it.bound_cu*ones(r,1);
         Tl1 = zeros(r);
         Tu1 = zeros(r);
         for i = 1:r
-            if tl(i) ~= 0
-            Tl1(i,i) = 1/tl(i);
+            if it.tl(i) ~= 0
+            Tl1(i,i) = 1/it.tl(i);
             end
-            if tu(i) ~= 0
-                Tu1(i,i) = 1/tu(i);
+            if it.tu(i) ~= 0
+                Tu1(i,i) = 1/it.tu(i);
             end
         end
 
         % helping variables
-        rhol = bound_cl*(C*x-cl-tl);
-        rhou = bound_cu*(-C*x + cu - tu);
-        betal = bound_xl*(x-xl-sl);
-        betau = bound_xu*(-x+xu-su);
-        Hphi = H + Sl1*Wl + Su1*Wu;
+        rhol = it.bound_cl*(nlp.C*it.x-nlp.cl-it.tl);
+        rhou = it.bound_cu*(-nlp.C*it.x + nlp.cu - it.tu);
+        betal = it.bound_xl*(it.x-nlp.xl-it.sl);
+        betau = it.bound_xu*(-it.x+nlp.xu-it.su);
+        Hphi = nlp.H + Sl1*Wl + Su1*Wu;
         psi = Tl1*Zl +Tu1*Zu;
-        xi = -bound_cl*(zl + Tl1*Zl*rhol) + bound_cu*(zu + Tu1*Zu*rhou);
+        xi = -it.bound_cl*(it.zl + Tl1*Zl*rhol) + it.bound_cu*(it.zu + Tu1*Zu*rhou);
         foo = psi\xi;
 
         % calculate affine step with reduced system
-        mat = [Hphi A' C';
-            A zeros(m,m + r);
-            C zeros(r,m) inv(psi)];
-        omega = [-H*x - c + A'*y + C'*bound_cl*zl - C'*bound_cu*zu - bound_xl*Sl1*Wl*betal + bound_xu*Su1*Wu*betau;
-            -(A*x-b);
+        mat = [Hphi nlp.A' nlp.C';
+            nlp.A zeros(m,m + r);
+            nlp.C zeros(r,m) inv(psi)];
+        omega = [-nlp.H*it.x - nlp.c + nlp.A'*it.y + nlp.C'*it.bound_cl*it.zl - nlp.C'*it.bound_cu*it.zu - it.bound_xl*Sl1*Wl*betal + it.bound_xu*Su1*Wu*betau;
+            -(nlp.A*it.x-nlp.b);
             foo];
         sol = mat\omega;
 
@@ -144,41 +111,29 @@ function [x, sl, su, tl, tu, y, wl, wu, zl, zu, bound_xl,bound_xu, bound_cl, bou
         del_z = -sol(n+m+1:n+m+r);
 
         % calculate all variables of expanded system
-        del_sl = bound_xl*(del_x + betal);
-        del_su = bound_xu*(-del_x + betau);
-        del_wl = bound_xl*(-wl -Sl1*Wl*del_sl);
-        del_wu = bound_xu*(-wu - Su1*Wu*del_su);
+        del_sl = it.bound_xl*(del_x + betal);
+        del_su = it.bound_xu*(-del_x + betau);
+        del_wl = it.bound_xl*(-it.wl -Sl1*Wl*del_sl);
+        del_wu = it.bound_xu*(-it.wu - Su1*Wu*del_su);
 
-        del_tl = bound_cl*(C*del_x + rhol);
-        del_tu = bound_cu*(-C*del_x + rhou);
-        del_zl = -bound_cl*(Tl1*Zl*del_tl + zl);
-        del_zu = bound_cu*(-del_z + del_zl);
+        del_tl = it.bound_cl*(nlp.C*del_x + rhol);
+        del_tu = it.bound_cu*(-nlp.C*del_x + rhou);
+        del_zl = -it.bound_cl*(Tl1*Zl*del_tl + it.zl);
+        del_zu = it.bound_cu*(-del_z + del_zl);
 
 
         % ensure positivity of s, w, t and z (interiority)
-        sl = bound_xl*max(abs(sl + del_sl),en);
-        su = bound_xu*max(abs(su + del_su),en);
+        it.sl = it.bound_xl*max(abs(it.sl + del_sl),en);
+        it.su = it.bound_xu*max(abs(it.su + del_su),en);
 
-        wl = bound_xl*max(abs(wl + del_wl),en);
-        wu = bound_xu*max(abs(wu + del_wu),en);
+        it.wl = it.bound_xl*max(abs(it.wl + del_wl),en);
+        it.wu = it.bound_xu*max(abs(it.wu + del_wu),en);
 
-        tl = bound_cl*max(abs(tl + del_tl),ones(r,1));
-        tu = bound_cu*max(abs(tu + del_tu),ones(r,1));
+        it.tl = it.bound_cl*max(abs(it.tl + del_tl),er);
+        it.tu = it.bound_cu*max(abs(it.tu + del_tu),er);
 
-        zl = bound_cl*max(abs(zl + del_zl),ones(r,1));
-        zu = bound_cu*max(abs(zu + del_zu),ones(r,1));
+        it.zl = it.bound_cl*max(abs(it.zl + del_zl),er);
+        it.zu = it.bound_cu*max(abs(it.zu + del_zu),er);
         
-        % Start
-        % x = start(1:n);
-        % sl = start(n+1:2*n);
-        % su = start(2*n+1:3*n);
-        % wl = start(3*n+1:4*n);
-        % wu = start(4*n+1:5*n);
-        % tl = start(5*n+1:5*n+r);
-        % tu = start(5*n+r+1:5*n+2*r);
-        % zl = start(5*n+2*r+1:5*n+3*r);
-        % zu = start(5*n+3*r+1:5*n+4*r);
-
-    end
     
 end
