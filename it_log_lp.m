0a0a4de7739e2ced0dd5ac537bfd6a895f64eb24
Loki <lar.icking@srabmit.sra.uni-hannover.de>
2025-09-05 15:30:47 +0200

    construct then struct for each iteration that will be put in the iteration log

---
+++ it_log_lp.m
@@ -0,0 +1,18 @@
+function [data] = it_log_lp(iter, it, nlp, sigma, alpha_pri, alpha_dual , help, dim)
+% Iteration Log 
+en = ones(dim.n,1);
+er = ones(dim.r,1);
+
+data.iter = iter;
+data.dual_gap = nlp.c'*it.x - (nlp.b'*it.y +nlp.xl'*it.wl -nlp.xu'*it.wu + nlp.cl'*it.zl -nlp.cu'*it.zu);
+pri_fea = [nlp.A*it.x-nlp.b; nlp.C*it.x - nlp.cl - it.tl; ...
+    -nlp.C*it.x + nlp.cu - it.tu; it.x - nlp.xl - it.sl; -it.x + nlp.xu - it.su];
+data.pri_fea = norm(pri_fea);
+data.dual_fea = norm(nlp.c - nlp.A'*it.y - nlp.C'*(it.zl - it.zu) - (it.wl -it.wu));
+compl = [help.Wl *it.sl - sigma*it.mu*en; help.Wu *it.su - sigma*it.mu*en;...
+    help.Zl*it.tl - sigma*it.mu*er; help.Zu*it.tu - sigma*it.mu*er];
+data.compl = norm(compl);
+data.alpha_p = alpha_pri;
+data.alpha_d = alpha_dual;
+
+end
